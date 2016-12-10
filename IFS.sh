5e64c7ef2ffa008b57b222bb4b198daee7d4cf35
snashraf <syednajeebashraf@gmail.com>
2016-12-10 16:13:47 +0300

    Testing git

---
+++ IFS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+names=(
+  "Aaron Maxwell"
+  "Wayne Gretzky"
+  "David Beckham"
+  "Anderson da Silva"
+)
+
+echo "With default IFS value..."
+for name in ${names[@]}; do
+  echo "$name"
+done
+
+echo ""
+echo "With strict-mode IFS value..."
+IFS=$'\n\t'
+for name in ${names[@]}; do
+  echo "$name"
+done
